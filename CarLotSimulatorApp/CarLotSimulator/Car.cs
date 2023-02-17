6f4e527c2a92f60f330e4ccadbc40602cc96d9c4
git-voltsoftware <info@volt.software>
2023-02-17 14:30:53 -0500

    need to touch up on this.

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+
+        public Car() { } // Constructor
+
+
+        //properties
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise()
+            {
+                Console.WriteLine("The " + Make + " " + Model + " engine goes " + EngineNoise);
+            }
+
+        public void MakeHonkNoise()
+        {
+                Console.WriteLine("The " + Make + " " + Model + " honk goes " + HonkNoise);
+        }
+        
+
+
+
+    }
+}
---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public CarLot() { } // Constrtuctor
+
+        public List<Car> cars { get; set; }
+
+    }
+}
